5f216404fa08bcfa519cb913c7c9a410c0e356cf
RebornTrack970 <62565267+RebornTrack970@users.noreply.github.com>
2021-03-07 18:41:16 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,21 @@
-//This is the js code that adds the servers:
+//I Suck at js so the servers get added and removed every tick. Pls someone help. aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa ;-;
+var ohno = 0;
 Events.on(Trigger.update.class, () => {
-
+  //delets all servers
+  
+  if(ohno != 1)
+  {
+    const allServers = Vars.defaultServers;
+    ohno = 1;
+  }
+  
+  Vars.defaultServers.clear();
+//Adds all servers.
 //Example \/
-  Vars.defaultServers.remove(ServerGroup("Modded Betamindy Server", ["185.86.230.102:25603"]));
+  Vars.defaultServers.add(allServers);
   Vars.defaultServers.add(new ServerGroup("Modded Betamindy Server", ["185.86.230.102:25603"]));
   
-  print("oh no");
+  
   
   
 //Dont mind the code after this:
