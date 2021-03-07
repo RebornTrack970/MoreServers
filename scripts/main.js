36e67557969d99956a6c39df516d6bb5af7e348d
RebornTrack970 <62565267+RebornTrack970@users.noreply.github.com>
2021-03-07 17:58:51 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,10 @@
 //This is the js code that adds the servers:
-Events.on(FileTreeInitEvent, () => {
+Events.on(ClientLoadEvent, () => {
 //Example \/
   
   Vars.defaultServers.add(new ServerGroup("Modded Betamindy Server", ["185.86.230.102:25603"]));
+  
+  print("oh no");
   
   
 //Dont mind the code after this:
