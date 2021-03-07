dcb272e391d3989da2e02208b3dd165b4dc83306
RebornTrack970 <62565267+RebornTrack970@users.noreply.github.com>
2021-03-07 18:19:47 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 //This is the js code that adds the servers:
 Events.on(ClientLoadEvent, () => {
+  var t = 0
+  while(t < 50) {
 //Example \/
   
   Vars.defaultServers.add(new ServerGroup("Modded Betamindy Server", ["185.86.230.102:25603"]));
@@ -8,4 +10,5 @@ Events.on(ClientLoadEvent, () => {
   
   
 //Dont mind the code after this:
+  }
 });
